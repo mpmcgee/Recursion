79e7020ce5163cd95d460a0ed7a935e67948fc66
mpmcgee <mpmcgee@iu.edu>
2022-01-05 21:24:26 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+int decimalToBinary(int);
+bool palindromeCheck(std::string, int, int);
+int binaryToDecimal(std::string, int i = 0);
+std::string reverseString(std::string);
+std::string keyEncode(std::string, int);
+//std::string formatString(std::string);
+
+int main() {
+    std::cout << "Recursion Projects" << std::endl;
+    //std::cout << decimalToBinary(5);
+//    std::string word = "racecar";
+//    std::cout << palindromeCheck(word, 0, word.length()-1);
+    //std::cout << binaryToDecimal("1110101010");
+    std::cout << keyEncode("hello", 27);
+
+    return 0;
+}
+
+//std::string formatString(std::string str) {
+//    return tolower(remove(str.begin(), str.end(), " "));
+//}
+
+int decimalToBinary(int decimal) {
+    if (decimal == 0) {
+        return 0;
+    }
+
+    return (10 * decimalToBinary(decimal / 2) + (decimal % 2));
+}
+
+int binaryToDecimal(std::string binary, int i) {
+    int n = binary.length();
+    if (i == n-1) {
+        return binary[i] - '0';
+    }
+    return ((binary[i] - '0') << (n-i-1)) + binaryToDecimal(binary, i+1);
+}
+
+
+
+bool palindromeCheck(std::string word, int start, int end) {
+    if (start >= end) {
+        return true;
+    }
+
+    if (word.at(start) != word.at(end)) {
+        return false;
+    }
+
+    return palindromeCheck(word, ++start, --end);
+}
+
+std::string reverseString(std::string str) {
+    if (str.length() == 1) {
+        return str;
+    }
+
+    return str.at(str.length() - 1) + reverseString(str.substr(0 ,str.length() - 1));
+}
+
+std::string keyEncode(std::string str, int key) {
+    if (str.length() == 0) {
+        return str;
+    }
+    return (char((str.at(0) + key - 97) % 26 + 97) + keyEncode(str.substr(1, str.length() - 1), key));
+}
+
